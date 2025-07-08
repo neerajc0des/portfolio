feb486fd074417342c3f4b386a40a12aae99fcf0
Neeraj Kumar <neerajkumara2525@gmail.com>
2025-07-08 19:32:37 +0530

    about section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { MessageCircle, CircleDot, Link } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -7,8 +9,8 @@ export default function Home() {
       <header className="navContainer hidden sm:flex items-center justify-center fixed top-10 z-50  px-4 py-3 w-full">
         <Navbar />
       </header>
-      <div className="main-container sm:px-6 lg:px-8 relative max-w-2xl pt-[40px] sm:pt-[130px] md:pt-[150px] lg:pt-[188px] mx-auto flex flex-col justify-center px-4 gap-8">
-        <div className="pfp-container w-full">
+      <div className="main-container sm:px-6 lg:px-10 relative max-w-2xl pt-[40px] sm:pt-[130px] md:pt-[150px] lg:pt-[200px] mx-auto flex flex-col justify-center px-4 gap-8">
+        <div className="pfp-container w-full pt-5">
           <img src="/pfp.jpg" className="w-24 h-24 rounded-full" alt="avatar" />
         </div>
 
@@ -22,6 +24,39 @@ export default function Home() {
             I do both. Turning ideas into products for last 1 year.
           </p>
         </div>
+
+        <div className="cta-container flex gap-5 items-center">
+          <Button type="button" className="cursor-pointer py-6 w-auto px-10 rounded-xl"><MessageCircle /> Send Message</Button>
+          <Button type="button" variant="default" className="py-5 bg-green-100 text-[#1D6D0B] rounded-full hover:bg-green-200 text-xs md:text-sm"><CircleDot /> Open to opportunities</Button>
+        </div>
+
+        <section className="aboutSection flex flex-col gap-5 mt-5 md:mt-10">
+          <h1 className="sectionHeader text-xl md:text-2xl font-semibold">About</h1>
+          <div className="sectionContent text-muted">
+            I'm Neeraj. Software dev based in Delhi, born in Madhya Pradesh.
+            Currently pursuing MCA at Delhi University.
+            <br /><br />
+            I've worked as a frontend developer for 6 months, shipping real-world products and collaborating closely with cross-functional teams. I care about the details — every pixel, every transition, every interaction.
+            <br /><br />
+            Most developers ignore design. Most designers avoid code. I live in the space between. I believe great UI isn't just about looks — it's about how it feels.
+            <br /><br />
+            I don't just build interfaces. I craft interfaces that work.
+          </div>
+        </section>
+
+        <div className="h-px w-full my-5 lg:my-10 bg-zinc-200"></div>
+
+        <section className="expSection flex flex-col gap-5">
+          <h1 className="sectionHeader text-xl md:text-2xl font-semibold">Work Experience</h1>
+          <div className="sectionContent text-muted space-y-2">
+            <div className="workItem flex">
+              <div className="duration">Mar 2025 - Present</div>
+              <div className="work">
+                <p className="text-paragraph">Co-Founder at</p> <Button><Link/></Button>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,32 +8,32 @@ const Navbar = () => {
   return (
     <div className=''>
       <div className="flex items-center pl-4 p-2 rounded-2xl border border-zinc-200 bg-white/70 backdrop-blur-lg text-zinc-700 w-fit">
-        <a href="/" className='hover:opacity-80 pl-3'>
-          <House strokeWidth='2'  size={23}/>
+        <a href="/" title="Home" className='hover:opacity-80 pl-3'>
+          <House strokeWidth='2'  size={22}/>
         </a>
-        <div className="w-px h-5 bg-zinc-200 mx-8"></div>
+        <div className="w-px h-5 bg-zinc-200 mx-6"></div>
         <div className="space-x-8 flex items-center">
-          <a href="/" className='hover:opacity-80'>
-            <Github strokeWidth='2'  size={23}/>
+          <a href="/" title="Github" className='hover:opacity-80'>
+            <Github strokeWidth='2'  size={22}/>
           </a>
 
-          <a href="/" className='hover:opacity-80'>
-            <Instagram strokeWidth='2'  size={23}/>
+          <a href="/" title="Instagram" className='hover:opacity-80'>
+            <Instagram strokeWidth='2'  size={22}/>
           </a>
 
-          <a href="/" className='hover:opacity-80'>
-            <Linkedin strokeWidth='2'  size={23}/>
+          <a href="/" title="Linkedin" className='hover:opacity-80'>
+            <Linkedin strokeWidth='2'  size={22}/>
           </a>
-          <a href="/" className='hover:opacity-80'>
-            <Sparkles strokeWidth='2' size={23}/>
+          <a href="/" title="Projects" className='hover:opacity-80'>
+            <Sparkles strokeWidth='2' size={22}/>
           </a>
 
-          <a href="/" className='hover:opacity-80'>
-            <ScrollText strokeWidth='2'  size={23}/>
+          <a href="/" title="Blog" className='hover:opacity-80'>
+            <ScrollText strokeWidth='2'  size={22}/>
           </a>
         </div>
         <div className="w-px h-5 bg-zinc-200 mx-8"></div>
-        <Button type="button" variant={'default'} className='cursor-pointer py-6 min-w-[180px] text-base'>
+        <Button type="button" variant={'default'} className='cursor-pointer rounded-xl py-6 min-w-[190px] text-base'>
           <Users size={'25px'} /> Visitor notes
         </Button>
       </div>
